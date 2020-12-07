6afcb922b37a415a61cd1363e52ae6d439c9dee1
= <=>
2020-12-07 18:20:16 +0100

    Task solved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 import java.util.Scanner;
 
+/*
+Fizz Buzz
+Fizz Buzz is a classic programming problem. Here is its slightly modified version.
+
+Write a program that takes the input of two integers: the beginning and the end of the interval (both numbers belong to the interval).
+
+The program should output the numbers from this interval, but if the number is divisible by 3, you should output Fizz instead of it; if the number is divisible by 5, output Buzz; and if it is divisible both by 3 and by 5, output FizzBuzz.
+
+Output each number or the word on a separate line.
+
+
+Sample Input:
+8 16
+
+Sample Output:
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
